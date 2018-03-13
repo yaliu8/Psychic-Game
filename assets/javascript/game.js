adc3ebb8f250fd8c69d9dda27dc602a56dfe4827
yaliu8 <yaliu8@gmail.com>
2018-03-12 23:57:19 -0700

    changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+
+var wins = 0
+var losses = 0
+var guessesLeft = 10
+var guessesSoFar = ""
+
+var userChoiceElement = document.getElementById('userChoice')
+var computerChoiceElement = document.getElementById('computerChoice')
+var winsElement = document.getElementById('wins')
+var lossesElement = document.getElementById('losses')
+var guessesLeftElement = document.getElementById('guessesLeft')
+var guessesSoFarElement = document.getElementById('guessesSoFar')
+
+var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+document.onkeyup = function(event) {
+  var userChoice = event.key.toLowerCase()
+  var randomIndex = Math.floor(Math.random() * choices.length)
+  var computerChoice = choices[randomIndex]
+
+if (userChoice === computerChoice) {
+    wins ++;
+    guessesSoFarElement.textContent = guessesSoFar += userChoice+", ";
+    }
+else if (userChoice !== computerChoice) {
+    guessesLeft --;
+    guessesSoFarElement.textContent = guessesSoFar += userChoice +", ";
+if (guessesLeft === 0) {
+    losses ++;
+    guessesLeft = 10;
+    guessesSoFarElement.textContent = guessesSoFar += userChoice+", ";
+
+}
+}
+    userChoiceElement.textContent = userChoice
+    computerChoiceElement.textContent = computerChoice
+    winsElement.textContent = wins
+    lossesElement.textContent = losses
+    guessesLeftElement.textContent = guessesLeft
+    guessesSoFarElement.textContent = guessesSoFar
+}
