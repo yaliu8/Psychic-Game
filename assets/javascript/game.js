acb529cf8ca10fd836f16ffc4910e0cad1e09519
Rai Lee <thisisrailee@gmail.com>
2018-03-16 16:42:29 -0700

    added some more logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,10 @@ var wins = 0
 var losses = 0
 var guessesLeft = 10
 var guessesSoFar = ""
+var userLetters = []
+var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var randomIndex = Math.floor(Math.random() * choices.length)
+var computerChoice = choices[randomIndex]
 
 var userChoiceElement = document.getElementById('userChoice')
 var computerChoiceElement = document.getElementById('computerChoice')
@@ -11,32 +15,38 @@ var lossesElement = document.getElementById('losses')
 var guessesLeftElement = document.getElementById('guessesLeft')
 var guessesSoFarElement = document.getElementById('guessesSoFar')
 
-var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+function start(){
+  guessesLeft = 10
+  randomIndex = Math.floor(Math.random() * choices.length);
+  computerChoice = choices[randomIndex];
+  guessesSoFar = "";
+  guessesSoFarElement.textContent = "";
+  userLetters = [];
+}
 
 document.onkeyup = function(event) {
+  console.log(computerChoice)
   var userChoice = event.key.toLowerCase()
-  var randomIndex = Math.floor(Math.random() * choices.length)
-  var computerChoice = choices[randomIndex]
-
-if (userChoice === computerChoice) {
-    wins ++;
-    guessesSoFarElement.textContent = guessesSoFar += userChoice+", ";
-    guessesSoFar = "";
+  if(choices.includes(userChoice)){
+    console.log("ran")
+    if (userChoice === computerChoice) {
+      wins ++;
+      start();
     }
-else if (userChoice !== computerChoice) {
-    guessesLeft --;
-    guessesSoFarElement.textContent = guessesSoFar += userChoice +", ";
-if (guessesLeft === 0) {
-    losses ++;
-    guessesLeft = 10;
-    guessesSoFarElement.textContent = guessesSoFar += userChoice+", ";
-    guessesSoFar = "";
-}
-}
-    userChoiceElement.textContent = userChoice
-    computerChoiceElement.textContent = computerChoice
-    winsElement.textContent = wins
-    lossesElement.textContent = losses
-    guessesLeftElement.textContent = guessesLeft
-    guessesSoFarElement.textContent = guessesSoFar
+    else if (userChoice !== computerChoice && !userLetters.includes(userChoice)) {
+      userLetters.push(userChoice)
+      guessesLeft --;
+      guessesSoFarElement.textContent = guessesSoFar += userChoice +", ";
+      if (guessesLeft === 0) {
+          losses ++;
+          start();
+      }
+    }
+  }
+  userChoiceElement.textContent = userChoice
+  computerChoiceElement.textContent = computerChoice
+  winsElement.textContent = wins
+  lossesElement.textContent = losses
+  guessesLeftElement.textContent = guessesLeft
+  guessesSoFarElement.textContent = guessesSoFar
 }
